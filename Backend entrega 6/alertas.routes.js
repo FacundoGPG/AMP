daf9283e46b823b30f7293c7e6ddc92b74f39194
SantyMCS <santymartin014@gmail.com>
2026-05-22 21:25:40 -0600

    Delete Backend entrega 6 directory

--- Backend entrega 6/alertas.routes.js
+++
@@ -1,7 +0,0 @@
-const verificarRol = require('../util/verificarRol');
-
-router.get(
-    "/alertas",
-    verificarRol(["Empleado", "Administrador"]),
-    alertasController.getAllAlertas
-);
--- Backend entrega 6/reportes.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportesController = require('../controllers/reportes.controller');
-
-router.get('/', reportesController.get_reportes);
-router.post('/crear', reportesController.post_crear);
-
-module.exports = router;
