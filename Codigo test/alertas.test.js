52345c5ce63ac09232fbe9cf0beb199e861b6a4f
FacundoGPG <fakgpineirog@gmail.com>
2026-05-26 14:49:16 -0600

    otra modificacion para ver si funciona el despliegue

--- Codigo test/alertas.test.js
+++
@@ -1,9 +0,0 @@
-describe('Modulo Alertas', () => {
-    test('crear alerta correctamente', () => {
-        const alerta = {
-            titulo: 'Alerta crítica',
-            prioridad: 'Alta'
-        };
-        expect(alerta.titulo).toBe('Alerta crítica');
-    });
-});
