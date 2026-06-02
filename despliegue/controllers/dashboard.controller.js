d57625cfe83d5ff24bc509bd5a457405bd007041
dvd8ddd <dacodmhuda0125@gmail.com>
2026-06-02 16:41:51 -0600

    Update dashboard.controller.js

--- despliegue/controllers/dashboard.controller.js
+++ despliegue/controllers/dashboard.controller.js
@@ -1,20 +1,14 @@
 const dashboardModel = require("../models/dashboard.model");
-
 exports.renderDashboard = async (req, res) => {
-  if (!req.session.usuario) {
-    return res.redirect("/");
-  }
-
-try {
+  try {
     const resumen = await dashboardModel.getResumen();
 
-    res.render("dashboard", {
+    return res.render("dashboard", {
       pageTitle: "Inicio - AMP",
       resumen
     });
   } catch (error) {
     console.error("Error cargando dashboard:", error);
-    res.status(500).send("Error al cargar el dashboard");
+    return res.status(500).send("Error al cargar el dashboard");
   }
 };
-
