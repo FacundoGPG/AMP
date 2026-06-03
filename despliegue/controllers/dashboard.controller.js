6d7e6ae078032d34c704ee9ea1f3985414fe648b
FacundoGPG <fakgpineirog@gmail.com>
2026-06-02 18:38:48 -0600

    se modificaron los valores de resumen para resolver el problema de vercel

--- despliegue/controllers/dashboard.controller.js
+++ despliegue/controllers/dashboard.controller.js
@@ -1,11 +1,11 @@
 const dashboardModel = require("../models/dashboard.model");
 exports.renderDashboard = async (req, res) => {
   try {
-    const resumen = await dashboardModel.getResumen();
+    // const resumen = await dashboardModel.getResumen();
 
     return res.render("dashboard", {
       pageTitle: "Inicio - AMP",
-      resumen
+      // resumen;
     });
   } catch (error) {
     console.error("Error cargando dashboard:", error);
