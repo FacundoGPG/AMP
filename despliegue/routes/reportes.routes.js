5b4226e26a3fd8c2dc7fcb9c67c15baabd243ac0
dvd8ddd <dacodmhuda0125@gmail.com>
2026-06-09 10:14:14 -0600

    reportes actualizados

--- despliegue/routes/reportes.routes.js
+++ despliegue/routes/reportes.routes.js
@@ -10,7 +10,12 @@ const ROLES_ADMIN = ["Administrador", "Oficial_Cumplimiento"];
 router.get("/", isAuth, verificarRol(ROLES_ADMIN), reportesController.get_reportes);
 router.post("/crear", isAuth, verificarRol(ROLES_ADMIN), reportesController.post_crear);
 router.post("/eliminar/:id", isAuth, verificarRol(ROLES_ADMIN), reportesController.delete_reporte);
+router.get("/exportar/xml", isAuth, verificarRol(ROLES_ADMIN), reportesController.exportarXML);
+router.get("/exportar/txt", isAuth, verificarRol(ROLES_ADMIN), reportesController.exportarTXT);
 router.get("/exportar/csv", isAuth, verificarRol(ROLES_ADMIN), reportesController.exportarCSV);
 router.get("/exportar/pdf", isAuth, verificarRol(ROLES_ADMIN), reportesController.exportarPDF);
+router.post("/enviar/:id", isAuth, verificarRol(ROLES_ADMIN), reportesController.enviarReporte);
+router.post("/estatus/:id", isAuth, verificarRol(ROLES_ADMIN), reportesController.getEstatusReporte);
+router.post("/estatus/:id", isAuth, verificarRol(ROLES_ADMIN), reportesController.actualizarEstatus);
 
 module.exports = router;
