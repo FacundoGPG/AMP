c77687a85583a49e3b0ae4b6e62b7e7fe94dbf68
dvd8ddd <dacodmhuda0125@gmail.com>
2026-06-09 21:18:38 -0600

    NUEVO controller de operaciones

--- despliegue/controllers/operations.controller.js
+++ despliegue/controllers/operations.controller.js
@@ -16,3 +16,25 @@ exports.getOperaciones = async (req, res) => {
     res.status(500).json({ error: "Error al obtener operaciones" });
   }
 };
+
+exports.getPerfilTransaccional = async (req, res)=>{
+  try{
+    const operaciones= await Operacion.getPerfilTransaccional();
+    res.json(perfil);
+  } catch (err){
+    console.error(err);
+    res.status(500).json({error: "Error al obtener perfil transaccional"});
+  }
+};
+
+exports.getOperacionesByCliente=async(req,res)=>{
+  try{
+    const{id}=req.params;
+    const operaciones=await Operacion.getOperacionesByCliente(id);
+    res.json(operaciones);
+  }catch(err){
+    console.error(err);
+    res.status(500).json({error: "Error al obtener operaciones del cliente"});
+  }
+};
+
