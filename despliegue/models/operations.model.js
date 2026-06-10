9cdf47057decf02454e3a4723ff04fe03f6b11f2
dvd8ddd <dacodmhuda0125@gmail.com>
2026-06-09 21:17:29 -0600

    subo operations.model es el bueno
    Added a new method to retrieve operations by client ID.

--- despliegue/models/operations.model.js
+++ despliegue/models/operations.model.js
@@ -33,4 +33,28 @@ exports.Operacion = class {
         const result = await pool.query(sql);
         return result.rows;
     }
+    //las o, c y p son abreviaturas
+    static async getOperacionesByCliente(id_cliente) {
+    const sql = `
+        SELECT
+            o.id_operacion AS "ID_Operacion",
+            o.tipo_operacion AS "Tipo_Operacion",
+            o.monto AS "Monto",
+            o.fecha AS "Fecha",
+            o.estado AS "Estado",
+            o.canal AS "Canal",
+            p.nombre AS "Producto"
+
+        FROM public."Operacion" o
+        JOIN public."Contrato" ct
+        ON ct.id_contrato = o.id_contrato
+        JOIN public."Producto" p
+        ON p.id_producto = ct.id_producto
+        WHERE ct.id_cliente = $1
+        ORDER BY o.fecha DESC
+    `;
+    const resultado = await pool.query(sql, [id_cliente]);
+    return resultado.rows;
 }
+}
+
