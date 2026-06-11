5fbabb5fa99009774768d038ec1ae6f77ab597a5
dvd8ddd <dacodmhuda0125@gmail.com>
2026-06-10 20:33:46 -0600

    subo alertas routes NUEVO

--- despliegue/routes/alertas.routes.js
+++ despliegue/routes/alertas.routes.js
@@ -8,5 +8,8 @@ const ROLES_ADMIN = ["Administrador", "Oficial_Cumplimiento"];
 
 router.get("/alertas", isAuth, verificarRol(ROLES_ADMIN), alertasController.renderAlertas);
 router.get("/api/alertas", isAuth, verificarRol(ROLES_ADMIN), alertasController.getAlertas);
+router.get("/api/alertas/operacion/:id", isAuth, verificarRol(ROLES_ADMIN), alertasController.getAlertasByOperacion);
+router.post("/api/alertas/:id/estatus", isAuth, verificarRol(ROLES_ADMIN), alertasController.updateEstatusAlerta);
+
 
 module.exports = router;
