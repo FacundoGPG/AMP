6e1318acf4dfe1965ad83983b907874b176d888e
dvd8ddd <dacodmhuda0125@gmail.com>
2026-06-12 15:15:11 -0600

    Update alertas.controller.js

--- despliegue/controllers/alertas.controller.js
+++ despliegue/controllers/alertas.controller.js
@@ -1,4 +1,5 @@
-const alertasModel = require("../models/alertas.model");
+const alertasModel= require("../models/alertas.model");
+//catch error sirve para agarrar errores y para evitar que se rompa 
 
 exports.renderAlertas = (req, res) => {
   res.render("alertas", {
@@ -6,12 +7,57 @@ exports.renderAlertas = (req, res) => {
   });
 };
 
-exports.getAlertas = async (req, res) => {
+//trae todas las alertas
+exports.getAlertas=async (req, res)=>{
   try {
-    const alertas = await alertasModel.getAlertas();
+    const alertas=await alertasModel.getAlertas();
     res.json(alertas);
-  } catch (error) {
-    console.error("Erorr al cargar Alertas", error);
-    res.status(500).json({ error: "Error al obtener alertas" })
+  } catch (err){
+    console.error(err);
+    res.status(500).json({error: "Error al obtener alertas"});
+  }
+};
+//op  especifica
+exports.getAlertasByOperacion=async (req, res)=>{
+  try{
+    const {id}= req.params;
+    const alertas= await alertasModel.getAlertasByOperacion(id);
+    res.json(alertas);
+  } catch(err){
+    console.error(err);
+    res.status(500).json({error: "Error al obtener alertas de la operacion"});
+  }
+};
+
+exports.updateEstatusAlerta= async (req, res)=>{
+  try{
+    const {id}=req.params;
+    const {estatus}=req.body;
+    const alerta=await alertasModel.updateEstatusAlerta(id, estatus);
+    if(!alerta){ //ya damos por hecho la existencia de la alerta
+      return res.status(404).json({error: "Alerta no encontrada"});
+    }
+    res.json(alerta);
+  }catch(err){
+    console.error(err);
+    if(err.message.includes("Estatus no válido")){ //include pregunta si lo tiene 
+      res.status(400).json({error: "Error al actualizar alerta"})
+    }else {
+      res.status(500).json({error:"Error al actualizar alerta"});
+    }
+  }
+};
+
+//historial
+//trae el historial de cambios de una alerta 
+//respuesta formato json
+exports.getHistorialAlerta = async(req,res)=>{
+  const id_alerta=req.params.id; //id del link
+  try{
+    const historial=await alertasModel.getHistorialAlerta(id_alerta);
+    res.json(historial);
+  }catch (err){
+    console.error(err);
+    res.status(500).json({ error: "Error al obetener el historial"});
   }
 };
