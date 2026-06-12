2a276a8476276608720b92a090aa8f4d1e2d2f18
dvd8ddd <dacodmhuda0125@gmail.com>
2026-06-12 15:14:34 -0600

    Update alertas.model.js

--- despliegue/models/alertas.model.js
+++ despliegue/models/alertas.model.js
@@ -1,7 +1,22 @@
-const pool = require("../config/database");
-
-exports.getAlertas = async () => {
-  const result = await pool.query(`
+const pool=require("../config/database");
+exports.getAlertas=async ()=>{
+  const result=await pool.query(`
+    SELECT 
+      id_alerta,
+      id_reporte,
+      tipo_alerta,
+      fecha_generacion.
+      motivo,
+      estatus,
+      prioridad,
+    FROM public."Alerta"
+    ORDER BY fecha_generacion DESC
+  `);
+  return result.rows;
+}
+//trae las listas de una operacion especifica
+exports.getAlertasByOperacion=async(id_operacion)=>{
+  const result= await pool.query(`
     SELECT
       a.id_alerta,
       a.tipo_alerta,
@@ -9,23 +24,42 @@ exports.getAlertas = async () => {
       a.motivo,
       a.estatus,
       a.prioridad,
-      COALESCE(cl.nombre, 'Sin cliente') AS cliente
+      aa.id_operacion,
     FROM public."Alerta" a
-    LEFT JOIN public."Alerta_Automatica" aa
-    ON
-      aa.id_alerta = a.id_alerta
-    LEFT JOIN public."Operacion" op
-    ON
-      op.ID_Operacion = aa.ID_Operacion
-    LEFT JOIN "Contrato" co
-    ON
-      co.ID_Contrato = op.ID_Contrato
-    LEFT JOIN public."Cliente" cl
-    ON
-      cl.ID_Cliente = co.ID_Cliente 
+    JOIN public."Alerta_Automatica" aa ON aa.id_alerta=a.id_alerta
+    WHERE aa.id_operacion=$1
     ORDER BY a.fecha_generacion DESC
-  `);
-
+    `, [id_operacion]);
   return result.rows;
+};
+
+exports.updateEstatusAlerta=async(id_alerta, estatus)=>{
+  const result=await pool.query(`
+    UPDATE public."Alerta"
+    SET estatus=$1
+    WHERE id_alerta=$2
+    RETURNING *
+  `,[id_alerta, estatus]);
+  return result.rows[0];
+};
+
+//historial
+//$1 se reemplaza con id_alerta
+//da el historial completo
+exports.getHistorialAlerta = async(id_alerta)=>{
+  const result=await pool.query(`
+    SELECT
+      id_historial,
+      id_alerta,
+      estado_anterior,
+      estado_nuevo,
+      accion,
+      usuario_responsable,
+      fecha_cambio
+    FROM public."Historial_Alerta"
+    WHERE id_alerta=$1
+    ORDER BY fecha_cambio DESC
+    `,[id_alerta]);
+    return result.rows;
 };
 
